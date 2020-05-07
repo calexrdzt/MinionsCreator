c18c01f0c2a82c780dbfcf9f9d8506d1571fe4b2
Christian Rodriguez <christianrodriguezt@gmail.com>
2020-05-06 19:18:29 -0500

    Updates

---
+++ precache-manifest.adb20332bc5732bc098adfd5ad50d227.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342d4339956a1a85cb925e2372d505cf",
+    "url": "/MinionsCreator/index.html"
+  },
+  {
+    "revision": "e0b19b81549578bea788",
+    "url": "/MinionsCreator/static/css/main.966cfc7b.chunk.css"
+  },
+  {
+    "revision": "e2e586fc41cce0f7cee2",
+    "url": "/MinionsCreator/static/js/2.e02c9d79.chunk.js"
+  },
+  {
+    "revision": "758f315ca4a196d985645f6e6df3f8f3",
+    "url": "/MinionsCreator/static/js/2.e02c9d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b19b81549578bea788",
+    "url": "/MinionsCreator/static/js/main.857a7f33.chunk.js"
+  },
+  {
+    "revision": "afdfa5bc7b2bd660fa9d",
+    "url": "/MinionsCreator/static/js/runtime-main.ddf18139.js"
+  },
+  {
+    "revision": "938e1acda014ee10dcdc95ff79430f61",
+    "url": "/MinionsCreator/static/media/MODERNA.938e1acd.TTF"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/MinionsCreator/static/media/Poppins-Medium.ba95810b.ttf"
+  }
+]);
